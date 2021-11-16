dba11fe66dda350d5f1a5fbbcea7a5e6fb53c20e
張立 <bellerophon00530@gmail.com>
2021-11-16 20:18:57 +0800

    set router config

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.static(path.join(__dirname, "build")));
+
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
